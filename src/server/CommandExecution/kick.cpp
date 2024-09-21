4aa1d3b98c9d4f1255ddf5e0cf7eb2d6f5023102
redarling <dresyv@gmail.com>
2024-09-21 14:01:32 +0200

    After kick testing, i fixed some bugs and commented things that are still doesn't work properly

--- src/server/CommandExecution/kick.cpp
+++ src/server/CommandExecution/kick.cpp
@@ -6,46 +6,53 @@ void    Server::kick(Client *client, const t_IRCCommand &command)
 
     if (!client->is_authenticated())
     {
-        this->_socket.send(fd, ERR_NOTREGISTERED);
-        return;
+        this->_socket.send(fd, ERR_NOTREGISTERED); // i don't recieve this message
+        return ;
     }
 
     if (command.params.size() < 2)
     {
-        this->_socket.send(fd, ERR_NEED_MORE_PARAMS);
-        return;
+        this->_socket.send(fd, ERR_NEED_MORE_PARAMS); // i don't recieve this message
+        return ;
     }
 
     std::string channelName = command.params[0];
-    std::string clientName = command.params[1];
+    std::string clientName  = command.params[1];
     std::string kickMessage = command.trailing.empty() ? command.trailing : client->getNickName();
-
-    Channel *channelToKickFrom = this->findChannel(channelName);
+    Channel     *channelToKickFrom = this->findChannel(channelName);
+    
     if (!channelToKickFrom)
     {
         this->_socket.send(fd, ERR_NO_SUCH_CHANNEL);
-        return;
+        return ;
     }
 
     if (!channelToKickFrom->isOperator(client))
     {
         this->_socket.send(fd, getMessage(client, ERR_CHANOPRIVSNEEDED, channelToKickFrom));
-        return;
+        return ;
     }
 
-    if (!this->isNickNameTaken(clientName))
-    {
-        this->_socket.send(fd, getMessage(client, ERR_NOSUCHNICK, channelToKickFrom));
-        return;
-    }
-
-    Client *clientToKick = this->findClientByNick(clientName);
+    Client      *clientToKick = this->findClientByNick(clientName);
     if (!clientToKick)
     {
-        this->_socket.send(fd, getMessage(client, ERR_USERNOTINCHANNEL, channelToKickFrom));
-        return;
+        this->_socket.send(fd, getMessage(client, ERR_USERNOTINCHANNEL, channelToKickFrom)); // i don't recieve this message
+        return ;
     }
 
-    this->broadcastMessage(client->getPrefix() + " KICK " + channelName + " " + clientName + " :" + kickMessage + "\r\n", channelToKickFrom); // raw🤮
-    // ABOVE KICK EXAMPLE: ":dan!d@localhost KICK #Melbourne alice :dan"
+    if (!channelToKickFrom->isMember(clientToKick))
+    {
+        this->_socket.send(fd, getMessage(client, ERR_USERNOTINCHANNEL, channelToKickFrom));
+        return ;
+    }
+
+    this->broadcastMessage
+    (clientToKick->getPrefix() + " KICK " + channelName + " " + clientName + " :" + kickMessage + "\r\n", channelToKickFrom); // raw
+    
+    channelToKickFrom->removeClient(clientToKick);
+
+    if (channelToKickFrom->getNbrUsers() == 0)
+    {
+        this->removeChannel(channelToKickFrom); // delete channel if it's empty
+    }
 }
